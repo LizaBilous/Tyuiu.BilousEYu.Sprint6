fcbd62b8f407c874b4c29a5e8e95bf58d8b7d96f
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2024-12-10 21:38:53 +0500

    6

--- Tyuiu.BilousEYu.Sprint6.Task6.V25.Lib/DataService.cs
+++ Tyuiu.BilousEYu.Sprint6.Task6.V25.Lib/DataService.cs
@@ -4,7 +4,7 @@ namespace Tyuiu.BilousEYu.Sprint6.Task6.V25.Lib
 {
     public class DataService : ISprint6Task6V25
     {
-        public string CollectTextFromFile(string str, string path)
+        public string CollectTextFromFile(string path)
         {
             string res = "";
             using (StreamReader reader = new StreamReader(path))
